4ed31d4907aee0f185fd063923a3c5c00c303924
yatabebe <c0b23143fb@edu.teu.ac.jp>
2024-11-28 12:53:51 +0900

    追加課題２:時間とともに爆弾を拡大、加速させる

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,10 +12,6 @@ DELTA = {
     pg.K_LEFT:(-5, 0), 
     pg.K_RIGHT:(+5, 0),
 }
-# accs = [a for a in range(1, 11)] #加速度のリスト
-# for r in range(1, 11): #拡大爆弾Surfaceのリスト
-#     bb_img = pg.Surface((20*r, 20*r))
-#     pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -37,6 +33,7 @@ def game_over(screen: pg.Surface) -> None:
     """
     ゲームオーバー時に、半透明の黒い画面上に「Game Over」と表示し、泣いているこうかとん画像を貼り付ける関数
     引数:screen Surface
+    戻り値:なし
     """
     naki_img = pg.image.load("fig/8.png") #泣いてるこうかとん画像
     naki_img = pg.transform.rotozoom(naki_img, 0, 0.9)
@@ -58,12 +55,20 @@ def game_over(screen: pg.Surface) -> None:
     time.sleep(5)
 
 
-# def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
-#     """
-#     サイズの異なる爆弾Surfaceを要素としたリストと加速度リストを返す
-#     引数:None
-#     """
-#     return accs, bomb_surfaces.append
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    """
+    サイズの異なる爆弾Surfaceを要素としたリストと加速度リストを返す
+    引数:None
+    戻り値:爆弾Surfaceのリストと加速度リストのタプル
+    """
+    bb_surface = []
+    accs = [a for a in range(1, 11)] #加速度のリスト
+    for r in range(1, 11): #拡大爆弾Surfaceのリスト
+        bb_img = pg.Surface((20*r, 20*r)) #空のSurface
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_img.set_colorkey((0, 0, 0)) #四隅の黒を透過させる
+        bb_surface.append(bb_img)
+    return bb_surface, accs
 
 
 def main():
@@ -73,11 +78,12 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200 #初期座標
-    bb_img = pg.Surface((20, 20)) # 爆弾用の空のSurface
+    bb_img = pg.Surface((20, 20)) #爆弾用の空のSurface
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) #爆弾円を描く
     bb_img.set_colorkey((0, 0, 0)) #四隅の黒を透過させる
-    bb_rct = bb_img.get_rect() #爆弾Rectの抽出
-    bb_rct.center = (random.randint(0, WIDTH), random.randint(0, HEIGHT)) #タプルでcenterに渡している
+    bb_imgs, bb_accs = init_bb_imgs() #爆弾のサイズと加速度を新しく設定する
+    bb_rct = bb_img.get_rect() #爆弾が存在する範囲を取得
+    bb_rct.center = (random.randint(0, WIDTH), random.randint(0, HEIGHT)) #タプルでランダムな中心位置をcenterに渡している
     vx, vy = +5, +5 #爆弾速度ベクトル
     clock = pg.time.Clock()
     tmr = 0
@@ -87,33 +93,33 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        #別のRectオブジェクトと重なっているか判定する      
         if kk_rct.colliderect(bb_rct):
             game_over(screen) #ゲームオーバー関数
             print("ゲームオーバー")
             return #ゲームオーバー
-        screen.blit(bg_img, [0, 0])
-
-        key_lst = pg.key.get_pressed()
+        screen.blit(bg_img, [0, 0]) #screenにbg_imgのSurfaceを貼りつける
+        key_lst = pg.key.get_pressed() #すべてのキーの押下状態を取得する
         sum_mv = [0, 0]
-        for key, tpl in DELTA.items(): #DELTAという辞書をfor文で回す #((K_UP, (0, -5))
+        #DELTAという辞書をfor文で回す((K_UP, (0, -5))
+        for key, tpl in DELTA.items():
             if key_lst[key]:
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
-        kk_rct.move_ip(sum_mv)
+        kk_rct.move_ip(sum_mv) #移動させる
         #こうかとんが画面外なら、元の場所に戻す
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-        screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy) #爆弾動く
+        screen.blit(kk_img, kk_rct) #screenにkk_imgのSurfaceを貼りつける
+        #爆弾拡大
+        avx = vx*bb_accs[min(tmr//500, 9)] #新しい加速度
+        bb_img = bb_imgs[min(tmr//500, 9)]
+        bb_rct.move_ip(avx, vy) #爆弾の速度
         yoko, tate = check_bound(bb_rct)
         if not yoko: #横にはみ出てる
             vx *= -1
         if not tate: #縦にはみ出てる
             vy *= -1
-        #爆弾拡大
-        #bb_imgs, bb_accs = init_bb_imgs()
-        #avx = vx*bb_accs[min(tmr//500, 9)]
-        #bb_img = bb_imgs[min(tmr//500, 9)]
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
